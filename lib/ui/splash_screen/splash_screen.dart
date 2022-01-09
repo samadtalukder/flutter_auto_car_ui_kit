db4358081e748ee366cbd475cefa68c2517e69f9
samad talukder <samadtalukdar6@gmail.com>
2022-01-09 18:11:04 +0600

    create splash screen class

---
+++ lib/ui/splash_screen/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:auto_car_ui_kit/image_path.dart';
+import 'package:auto_car_ui_kit/ui/on_board_screen/on_board_data_model.dart';
+import 'package:auto_car_ui_kit/ui/on_board_screen/on_board_item.dart';
+import 'package:auto_car_ui_kit/ui/on_board_screen/on_board_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 5),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnBoardScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              ImagePath.icCar,
+              color: Colors.white,
+              height: 120,
+            ),
+            Text(
+              "Auto Car".toUpperCase(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
