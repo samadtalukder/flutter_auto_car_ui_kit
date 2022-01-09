18c426791a779a6788d31e358455477efb292dff
samad talukder <samadtalukdar6@gmail.com>
2022-01-09 18:10:34 +0600

    create on board screen data model class

---
+++ lib/ui/on_board_screen/on_board_data_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../image_path.dart';
+
+class OnBoardDataModel {
+  final String onBoardTitle;
+  final String onBoardDescription;
+  final String onBoardImageSrc;
+  final Color onBoardBgColor;
+
+  OnBoardDataModel(this.onBoardTitle, this.onBoardDescription,
+      this.onBoardImageSrc, this.onBoardBgColor);
+}
+
+List<OnBoardDataModel> onBoardList = [
+  OnBoardDataModel(
+    "Find Your Dream Car",
+    "Find the car of your dreams from the worlds largest marketplace",
+    ImagePath.icCar,
+    Colors.white,
+  ),
+  OnBoardDataModel(
+    "Your Offer Is Being Proceed",
+    "Find the car of your dreams from the worlds largest marketplace",
+    ImagePath.icCar,
+    Colors.white,
+  ),
+
+];
